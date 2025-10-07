93f58b56a9ff9ffeddda71050a3b17cbc2e63426
Louis Policarpio <94048557+locwa@users.noreply.github.com>
2025-10-07 13:05:05 +0800

    reverted files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TaskFormComponent } from './task-form/task-form.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { SummarySectionComponent } from './summary-section/summary-section.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskFormComponent, TaskListComponent, SummarySectionComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
